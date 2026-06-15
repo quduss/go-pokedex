20d3a611b74282dac5c00ebbd0f1f33fa7e83a36
quduss <qudshuaib@gmail.com>
2026-06-15 21:11:53 +0100

    Create a method that adds a new entry to the cache

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -22,3 +22,13 @@ func NewCache(interval time.Duration) Cache {
 
 	return c
 }
+
+// Add -
+func (c *Cache) Add(key string, value []byte) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	c.cache[key] = cacheEntry{
+		createdAt: time.Now().UTC(),
+		val:       value,
+	}
+}
