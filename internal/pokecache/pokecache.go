e29521e2b73dc0b564a2b358c14e37b3ed3dcc79
quduss <qudshuaib@gmail.com>
2026-06-15 21:36:59 +0100

    Create a method that removes any entries that are older than the interval passed to newly created cache

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -32,3 +32,18 @@ func (c *Cache) Add(key string, value []byte) {
 		val:       value,
 	}
 }
+
+// Get -
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	val, ok := c.cache[key]
+	return val.val, ok
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.reap(time.Now().UTC(), interval)
+	}
+}
