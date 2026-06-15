f7b3c42a167da951e19d2e46e6926e211984fdcf
quduss <qudshuaib@gmail.com>
2026-06-15 21:03:41 +0100

    Write a function that creates a new cache with a configurable interval

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -5,3 +5,20 @@ type Cache struct {
 	cache map[string]cacheEntry
 	mux   *sync.Mutex
 }
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+// NewCache -
+func NewCache(interval time.Duration) Cache {
+	c := Cache{
+		cache: make(map[string]cacheEntry),
+		mux:   &sync.Mutex{},
+	}
+
+	go c.reapLoop(interval)
+
+	return c
+}
