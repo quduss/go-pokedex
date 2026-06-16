e7c10e553f02d19200e72158c4311a0f15a0428b
quduss <qudshuaib@gmail.com>
2026-06-16 22:28:05 +0100

    Define a reap method that deletes stale entries from the cache

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -1,5 +1,9 @@
+package pokecache
 
-
+import (
+	"sync"
+	"time"
+)
 
 type Cache struct {
 	cache map[string]cacheEntry
@@ -45,5 +49,15 @@ func (c *Cache) reapLoop(interval time.Duration) {
 	ticker := time.NewTicker(interval)
 	for range ticker.C {
 		c.reap(time.Now().UTC(), interval)
+	}
+}
+
+func (c *Cache) reap(now time.Time, last time.Duration) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	for k, v := range c.cache {
+		if v.createdAt.Before(now.Add(-last)) {
+			delete(c.cache, k)
+		}
 	}
 }
